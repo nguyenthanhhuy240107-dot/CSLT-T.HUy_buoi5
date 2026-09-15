ab2318f27fd373067ddb96475ecb1a9919de0cbf
nguye <nguye@HYRO>
2026-09-15 12:53:52 +0700

    buổi5

---
+++ Ex.6/Ex.6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Số lượng phần tử: ");
+            int n = int.Parse(Console.ReadLine());
+            double tong = 0.0;
+
+            Console.WriteLine("Dãy số điều hòa:");
+            for (int i = 1; i <= n; i++)
+            {
+                tong += 1.0 / i;
+                Console.Write("1/{0}", i);
+                if (i < n)
+                    Console.Write(" + ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Tổng của dãy số  = {tong}");
+        }
+    }
+}
---
+++ Ex.7/Ex.7.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Khoảng dưới: ");
+            int lower = int.Parse(Console.ReadLine());
+            Console.Write("Khoảng trên: ");
+            int upper = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Các số hoàn hảo trong khoảng ({lower}-{upper}) là:");
+
+            for (int num = lower; num <= upper; num++)
+            {
+                int tongUoc = 0;
+
+                for (int i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                        tongUoc += i;
+                }
+
+                if (tongUoc == num)
+                    Console.WriteLine(num);
+            }
+        }
+    }
+}
